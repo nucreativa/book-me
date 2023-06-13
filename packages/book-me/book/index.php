3e19ba88cadc99c5a91bc4cbec9a06f5d16e2471
Ary Wibowo <ary.wibowo@brinks.com>
2023-06-13 19:59:08 +0700

    Refactor

--- packages/book-me/book/index.php
+++ packages/book-me/book/index.php
@@ -7,10 +7,12 @@ function main(array $args): array
 {
   $pheanstalk = Pheanstalk::create(getenv('beanstalkd_host'));
 
+  $now = Carbon::now()->minute(0)->second(0)->setTimezone('Asia/Jakarta');
+
   $book = [
     'date' => $args['date'] ?? date('Y-m-d'),
-    'start' => $args['start'] ?? Carbon::now()->minute(0)->second(0)->toTimeString(),
-    'end' => $args['end'] ?? Carbon::now()->addHour()->minute(0)->second(0)->toTimeString()
+    'start' => $args['start'] ?? $now->toTimeString(),
+    'end' => $args['end'] ?? $now->addHour()->toTimeString()
   ];
 
   // Queue a Job
